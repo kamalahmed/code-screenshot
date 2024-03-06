6b9a0eaaf4ef8d1a81bc2db4dfc151aa7c4233c6
Kamal <kamalacca@gmail.com>
2024-03-06 09:08:11 +0400

    finish first version of the app and add vercel config

--- app.js
+++ app.js
@@ -1,105 +1,105 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
-const multer = require('multer');
+const multer = require("multer");
 
 const app = express();
 const upload = multer(); // for parsing multipart/form-data
 const port = 3000;
 
-// Set up EJS as the view engine
-app.set("view engine", "ejs");
-
-// Serve static files from the public directory
+//Middlewares
+// for serving static files from the public directory
 app.use(express.static("public"));
-
-// Parse request bodies
+// for parsing request bodies
 app.use(express.urlencoded({ extended: true }));
-// Home
-app.get("/", (req, res) => {
-  res.render("index");
-});
+
 
 app.post("/screenshot", upload.none(), async (req, res) => {
   const code = req.body.code;
-  const textareaHeight = parseInt(req.body.textareaHeight, 10);
-  const textareaWidth = parseInt(req.body.textareaWidth, 10);
+  // escape HTML entities so that browser treats them as plain text
+  const escapedHTML = code.replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#039;');
 
-  console.log(textareaHeight);
-  console.log(textareaWidth);
+  // get dynnamic height and width from the html code editor
+  const editorHeight = parseInt(req.body.editorHeight, 10);
+  const editorWidth = parseInt(req.body.editorWidth, 10);
+
 
   // Launch a new browser instance
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-    const width = textareaWidth? textareaWidth + 100 : 700;
-    const height = textareaHeight? textareaHeight + 100 : 500;
+  const width = editorWidth ? editorWidth + 100 : 700;
+  const height = editorHeight ? editorHeight + 100 : 500;
   // Set the viewport based on the textarea dimensions
   await page.setViewport({ width, height });
 
-
   // Load the code snippet template and set the code content
   await page.setContent(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>Code Screenshot</title>
-          <link rel="stylesheet" href="/style.css" />
-          <style>
-          body {
-            font-family: 'Menlo', monospace;
-            padding: 20px;
-            color: #abb2bf;
-          }
-          
-          pre {
-            background-color: #282c34;
-            border-radius: 10px;
-            overflow: auto;
-            position: relative;
-          }
-          
-          code {
-            white-space: pre-wrap;
-          }
-          
-          .traffic-lights {
-            position: absolute;
-            top: 10px;
-            left: 10px;
-            display: flex;
-          }
-          
-          .traffic-light {
-            width: 12px;
-            height: 12px;
-            border-radius: 50%;
-            margin-right: 5px;
-          }
-          
-          .red {
-            background-color: #ff5f56;
-          }
-          
-          .yellow {
-            background-color: #ffbd2e;
-          }
-          
-          .green {
-            background-color: #27c93f;
-          }
-          </style>
-        </head>
-        <body>
-          <pre>
-            <div class="traffic-lights">
-              <div class="traffic-light red"></div>
-              <div class="traffic-light yellow"></div>
-              <div class="traffic-light green"></div>
-            </div>
-            <code>${code}</code>
-          </pre>
-        </body>
-      </html>
-    `);
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Code Screenshot</title>
+    <style>
+    body {
+      font-family: 'Menlo', monospace;
+      padding: 20px;
+      color: #fff;
+    }
+
+    pre {
+      padding: 30px 20px 20px;
+      background-color: #282c34;
+      border-radius: 10px;
+      overflow: auto;
+      position: relative;
+    }
+
+    code {
+      white-space: pre-wrap;
+    }
+
+    .window-lights {
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      display: flex;
+    }
+
+    .window-light {
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+      margin-right: 5px;
+    }
+
+    .w-red {
+      background-color: #ff5f56;
+    }
+
+    .w-yellow {
+      background-color: #ffbd2e;
+    }
+
+    .w-green {
+      background-color: #27c93f;
+    }
+    </style>
+  </head>
+  <body>
+    <pre>
+        <div class="window-lights">
+            <div class="window-light w-red"></div>
+            <div class="window-light w-yellow"></div>
+            <div class="window-light w-green"></div>
+        </div>
+        <code>${escapedHTML}</code>
+    </pre>
+  </body>
+</html>
+`);
+
 
   // Take a screenshot of the page
   const screenshot = await page.screenshot();
